7fe64fbb450e1e148c5c3ffe25ca4a46094c9a01
John Steel <john@jskw.dev>
2024-08-25 20:12:59 -0400

    docker build tag push

--- src/test/java/dev/jskw/tomorrowkey/TomorrowKeyApplicationTests.java
+++ src/test/java/dev/jskw/tomorrowkey/TomorrowKeyApplicationTests.java
@@ -2,12 +2,22 @@ package dev.jskw.tomorrowkey;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class TomorrowKeyApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public String exampleBean() {
+            return "example";
+        }
     }
 
 }
